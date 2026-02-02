ddcc5847ce6fb3b018090c7c481fab6cabbe79f3
callarose4 <crkolar@syr.edu>
2026-02-02 18:28:26 +0000

    updated again

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ lab2_page = st.Page("Labs/Lab2.py", title="Lab 2", default=True) #default to Lab
 lab3_page = st.Page("Labs/Lab3.py", title="Lab 3")
 
 
-pg = st.navigation([lab1_page, lab2_page])
+pg = st.navigation([lab1_page, lab2_page, lab3_page])
 pg.run()
