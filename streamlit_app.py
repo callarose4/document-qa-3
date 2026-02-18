09f13223b8918de7f8eb66f30dc9e37ad7140c74
callarose4 <crkolar@syr.edu>
2026-02-18 18:52:33 +0000

    deleted default expression

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ lab1_page = st.Page("Labs/Lab1.py", title="Lab 1")
 lab2_page = st.Page("Labs/Lab2.py", title="Lab 2")
 lab3_page = st.Page("Labs/Lab3.py", title="Lab 3")
 lab4_page = st.Page("Labs/Lab4.py", title="Lab 4")
-lab5_page = st.Page("Labs/Lab5.py", title="Lab 5", default=True)
+lab5_page = st.Page("Labs/Lab5.py", title="Lab 5")
 
 
 pg = st.navigation([lab1_page, lab2_page, lab3_page, lab4_page, lab5_page], default=lab5_page)
